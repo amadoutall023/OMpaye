f6111b12ac9e49ea1b9ed403d584901b68b9fdb3
amadoutall023 <elhadjiamadoutall85@gmail.com>
2025-11-11 12:28:31 +0000

    moidq

--- config/cors.php
+++ config/cors.php
@@ -25,6 +25,18 @@ return [
 
     'allowed_headers' => ['*'],
 
+    'allowed_methods' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
 
     'max_age' => 0,
